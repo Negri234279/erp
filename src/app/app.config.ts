8b4510d2d28321c9df89f9c7afd35cda771f4c6b
Negri234279 <rafalejo01@gmail.com>
2025-01-26 23:45:58 +0100

    Agregar configuración de Tailwind CSS y PrimeNG, actualizar estilos globales y optimizar el componente principal

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
-import { provideRouter } from '@angular/router'
-
-import { routes } from './app.routes'
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideRouter } from '@angular/router'
+import Aura from '@primeng/themes/aura'
+import { providePrimeNG } from 'primeng/config'
+import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideClientHydration(withEventReplay()),
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura,
+                options: {
+                    darkModeSelector: '.dark-mode',
+                },
+            },
+        }),
     ],
 }
